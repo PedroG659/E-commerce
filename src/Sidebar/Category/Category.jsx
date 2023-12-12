654859867050204a5c65d0834c380f6c2b641681
PedroG659 <150958889+PedroG659@users.noreply.github.com>
2023-12-12 17:47:59 -0300

    Update Category

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -2,7 +2,32 @@ import './Category.css'
 
 function Category() {
   return (
-    <div>Category</div>
+    <div>
+      <h2 className="sidebar-title">Categoria</h2>
+
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test' />
+          <span className="checkmark"></span>Todos
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test' />
+          <span className="checkmark"></span>Tênis
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test' />
+          <span className="checkmark"></span>Flats
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test' />
+          <span className="checkmark"></span>Sandálias
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test' />
+          <span className="checkmark"></span>Salto
+        </label>
+      </div>
+    </div>
   )
 }
 
