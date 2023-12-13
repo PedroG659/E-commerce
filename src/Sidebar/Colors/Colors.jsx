73f69eff1ae6c7b97c7d435984c22c14c95d27c7
PedroG659 <150958889+PedroG659@users.noreply.github.com>
2023-12-13 14:18:09 -0300

    Update Colors

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -2,7 +2,37 @@ import './Colors.css'
 
 function Colors() {
   return (
-    <div>Colors</div>
+    <div>
+            <label className='sidebar-label-container color-title'>
+        <input type="radio" name='test' />
+        <span className='checkmark'></span>Todos
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test' />
+        <span className='checkmark'></span>Preto
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test' />
+        <span className='checkmark'></span>Azul
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test' />
+        <span className='checkmark'></span>Vermelho
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test' />
+        <span className='checkmark'></span>Verde
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test' />
+        <span className='checkmark'></span>Branco
+      </label>
+    </div>
   )
 }
 
