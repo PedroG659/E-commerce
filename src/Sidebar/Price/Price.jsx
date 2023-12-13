bfc8c8728963298eca7d5b8e182dc8fc88721a48
PedroG659 <150958889+PedroG659@users.noreply.github.com>
2023-12-13 14:01:08 -0300

    Update Price

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -2,7 +2,35 @@ import './Price.css'
 
 function Price() {
   return (
-    <div>Price</div>
+    <div className='ml'>
+      <h2 className='sidebar-title price-title'>Preço</h2>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test' />
+        <span className='checkmark'></span>Todos
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test' />
+        <span className='checkmark'></span>R$0 - R$50
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test' />
+        <span className='checkmark'></span>R$50 - R$100
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test' />
+        <span className='checkmark'></span>R$100 - R$150
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test' />
+        <span className='checkmark'></span>Mais de R$150
+      </label>
+
+    </div>
   )
 }
 
