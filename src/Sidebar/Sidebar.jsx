20747587635dce6da1ed258643c35fffbc867983
ENS-PedroTimoteo <150958889+PedroG659@users.noreply.github.com>
2023-12-14 10:36:14 -0300

    Update

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -2,13 +2,14 @@ import Category from "./Category/Category";
 import Price from "./Price/Price";
 import Colors from "./Colors/Colors";
 import "./Sidebar.css";
+import { PiSneakerLight  } from "react-icons/pi";
 
 const Sidebar = ({ handleChange }) => {
   return (
     <>
       <section className="sidebar">
         <div className="logo-container">
-          <h1>🛒</h1>
+          <h1><PiSneakerLight  size={40}/></h1>
         </div>
         <Category handleChange={handleChange} />
         <Price handleChange={handleChange} />
